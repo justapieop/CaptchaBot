1c53049030592a79050b5619eee7b4112adedac7
JustAPie <dotranthaian2006@gmail.com>
2020-08-15 09:36:08 +0700

    modify something

--- commands/verification/verify.js
+++ commands/verification/verify.js
@@ -29,26 +29,39 @@ module.exports = {
         .setImage("attachment://captcha.png");
         
         const dmc = await message.author.createDM();
-        const msg = await dmc.send(embed);
-        
-        const filter = m => m.author.id === message.author.id;
+        await dmc.send(embed)
+        .then(async mess => {
+            const embed = new MessageEmbed()
+            .setColor("#1ef000")
+            .setDescription("Verification code sent! Please check your DM");
+            channel.send(embed);
+            const filter = m => m.author.id === message.author.id;
 
-        const collector = await dmc.createMessageCollector(filter);
+            const collector = await dmc.createMessageCollector(filter);
 
-        collector.on("collect", (m) => {
-            msg.delete();
-            if (m.content.toUpperCase() === captcha.value) {
-                const embed = new MessageEmbed()
-                .setColor("#1ef000")
-                .setDescription(`:tada: Verification Sucessfully! You are now having access to ${message.guild.name}`);
-                dmc.send(embed);
-                return authorUser.roles.add(role.id, "Verified");
-            } else {
-                const embed = new MessageEmbed()
-                .setColor("#1ef000")
-                .setDescription(`:tada: Verification Failed! Please try again!`);
-                return dmc.send(embed);
-            }
+            collector.on("collect", async function (m) {
+                mess.delete();
+                if (m.content.toUpperCase() === captcha.value) {
+                    let embed = new MessageEmbed()
+                    .setColor("#ffff00")
+                    .setDescription("🎉 Verification completed! You are now having access to the server");
+                    await dmc.send(embed);
+                    return authorUser.roles.add(role, "Verified");
+                } else {
+                    let embed = new MessageEmbed()
+                    .setColor("#ff0000")
+                    .setDescription("🎉 Verification failed! Please try again!");
+                    return message.channel.send(embed);
+                }
+            });
+        })
+        .catch(() => {
+            const embederr = new MessageEmbed()
+            .setColor("#ff0000")
+            .setDescription("An error occurred while sending captcha! Make sure you have DM enabled in the server's security settings");
+            return channel.send(embederr);
         });
+        
+        
     }
 };
