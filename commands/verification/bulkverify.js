bab2d6c3115a38cd8a0f01d848fee5ecbfbf03b9
JustAPie <dotranthaian2006@gmail.com>
2020-08-26 18:07:28 +0700

    Added two options to bulkverify command

--- commands/verification/bulkverify.js
+++ commands/verification/bulkverify.js
@@ -7,6 +7,13 @@ module.exports = {
     usage: "(command)",
     perms: "ADMINISTRATOR",
     async execute(client, message, args, settings) {
+        if ((!args.length) || args[0] !== "human" || "all") {
+            let embed = new MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("User input required")
+            .setDescription(`Usage: ${this.usage}`);
+            return message.channel.send(embed);
+        } 
         const role = message.guild.roles.cache.get(settings.GiveRole);
         if (!role) {
             let embed = new MessageEmbed()
@@ -17,12 +24,18 @@ module.exports = {
 
         let embed = new MessageEmbed()
         .setColor("#1ef000")
-        .setDescription("Verifying all members. This may take a while")
+        .setDescription("Verifying members. This may take a while")
         await  message.channel.send(embed);
 
-        await message.guild.members.cache.forEach(async member => {
-            await member.roles.add(role, "Bulk Verification");
-        });
+        if (args[0] === "human") {
+            await message.guild.members.cache.filter(m => !m.user.bot).forEach(async member => {
+                await member.roles.add(role, "Bulk Verification");
+            });
+        } else if (args[0] === "all") {
+            await message.guild.members.cache.forEach(async member => {
+                await member.roles.add(role, "Bulk Verification");
+            });
+        }
 
         embed.setDescription("All members are successfully verified");
         return message.channel.send(embed);
